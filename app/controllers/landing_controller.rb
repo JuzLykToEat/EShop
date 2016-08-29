fc9084f445419e9f2434abf847e1f3c18700a437
shahminrazali <shahminrazali@gmail.com>
2016-08-29 14:38:23 +0800

    another commit

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,11 @@
 class LandingController < ApplicationController
+<<<<<<< HEAD
   def index
   end
+=======
+
+  def index
+  end
+  
+>>>>>>> master
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
 
   root to: "landing#index"
+=======
+  root to: "landing#index"
+
+>>>>>>> master
 end
