35accdadce343537da5f1cfee5d032cbb8917720
Low Yeong Yih <low.yeongyih@hotmail.com>
2016-09-01 14:42:37 +0800

    removed extra columns in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831085457) do
+ActiveRecord::Schema.define(version: 20160901064106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160831085457) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -39,10 +39,8 @@ ActiveRecord::Schema.define(version: 20160831085457) do
     t.datetime "updated_at",                       null: false
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-
   end
 
 end
