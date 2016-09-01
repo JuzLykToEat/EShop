d383fbca6916f23935746d07d823645fa21d600c
shahminrazali <shahminrazali@gmail.com>
2016-09-01 18:48:59 +0800

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160901064106) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +61,6 @@ ActiveRecord::Schema.define(version: 20160901064106) do
     t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-
   end
 
 end
