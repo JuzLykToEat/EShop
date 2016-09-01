dcd5bcb38da1ec4025cbb82345d8cd8f7531973c
Low Yeong Yih <low.yeongyih@hotmail.com>
2016-09-01 15:20:24 +0800

    Added flash message for fb log in

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,8 +1,12 @@
 class OmniauthController < ApplicationController
   def create
     user_fb = User.from_omniauth(request.env["omniauth.auth"])
-    session[:id] = user_fb.id
-    redirect_to root_url
+
+    if user_fb
+      session[:id] = user_fb.id
+      flash[:success] = "Welcome back #{current_user.username}"
+      redirect_to root_path
+    end
   end
 
   def destroy
